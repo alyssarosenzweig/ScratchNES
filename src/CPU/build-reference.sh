f7caf843f5143c70666498c71921ac8f9e191d8e
Alyssa Rosenzweig <alyssa@backtick.town>
2016-06-22 10:40:30 -0700

    Tpyo

--- src/CPU/build-reference.sh
+++ src/CPU/build-reference.sh
@@ -5,7 +5,7 @@ ORIGIN=http://e-tradition.net/bytes/6502/6502_instruction_set.html
 
 # Fetch reference
 wget $ORIGIN
-mv 6502_instuction_set.html bin/reference.html
+mv 6502_instruction_set.html bin/reference.html
 
 # trim out plain text -- first 200 lines 
 tail -n +201 bin/reference.html | head -n 54 > bin/reference.txt
