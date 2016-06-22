95d3593c3d136c6db9c6452618ad1b620cd2f921
Alyssa Rosenzweig <alyssa@backtick.town>
2016-06-22 10:39:40 -0700

    oops

--- src/CPU/build-reference.sh
+++ src/CPU/build-reference.sh
@@ -4,7 +4,7 @@
 ORIGIN=http://e-tradition.net/bytes/6502/6502_instruction_set.html
 
 # Fetch reference
-wget ORIGIN
+wget $ORIGIN
 mv 6502_instuction_set.html bin/reference.html
 
 # trim out plain text -- first 200 lines 
