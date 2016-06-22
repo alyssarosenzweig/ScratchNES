01ed085abeea5f7efbe2782580a0c3034155e38e
Alyssa Rosenzweig <alyssa@backtick.town>
2016-06-22 10:37:34 -0700

    Build reference script

---
+++ src/CPU/build-reference.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Original copy of tyhe instruction set
+ORIGIN=http://e-tradition.net/bytes/6502/6502_instruction_set.html
+
+# Fetch reference
+wget ORIGIN
+mv 6502_instuction_set.html bin/reference.html
+
+# trim out plain text -- first 200 lines 
+tail -n +201 bin/reference.html | head -n 54 > bin/reference.txt
