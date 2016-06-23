60f74f5964cef9c97b7c012bd45026ca5e607102
Alyssa Rosenzweig <alyssa@backtick.town>
2016-06-23 15:16:16 -0700

    Meta file

--- src/CPU/tableify.js
+++ src/CPU/tableify.js
@@ -2,4 +2,13 @@
  * automatically fills up a buffer with stuff from text
  */
 
-console.log(require("fs").readFileSync(process.argv[2]).toString().split("\n").map(function(line) { return 'add "' + line + '" to ' + process.argv[3]; }).join("\n"));
+var arr = require("fs").readFileSync(process.argv[2])
+            .toString()
+             .split("\n")
+             .map(function(line) {
+                 return 'add "' + line + '" to ' + process.argv[3];
+             });
+
+console.log("when this sprite clicked");
+console.log("delete all of " + process.argv[3]);
+console.log(arr.join("\n"));
