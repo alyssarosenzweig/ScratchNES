aafd01a39557968a0c82b4fd6bab695479cffd72
Alyssa Rosenzweig <alyssa@backtick.town>
2016-06-23 07:50:12 -0700

    Fix TSX

--- src/CPU/build-transfer.js
+++ src/CPU/build-transfer.js
@@ -1,4 +1,5 @@
 /* builds TAX-family instructions */
+/* note these get a little funky because of differences in encoding */
 
 var fs = require("fs");
 
@@ -7,9 +8,22 @@ list.forEach(emit);
 
 function emit(name) {
     var emission = [
-        "IMPLIED",
-        "set " + name[0] + " to " + name[1]
+        "IMPLIED"
     ];
+
+    if(name.indexOf("S") > -1) {
+        if(name[0] == "S") {
+            // transfer stack in
+            // encode it to hex
+            emission.push("set " + name[0] + "to item (" + name[1] + "+1) of hex");
+        } else {
+            // transfer stack out
+            // decode hex
+            emission.push("set " + name[0] + "to (join \"0x\" (" + name[1] + "))");
+        }
+    } else {
+        emission.push("set " + name[0] + " to " + name[1]);
+    }
 
     fs.writeFileSync("instructions/T" + name, emission.join("\n"));
 }
