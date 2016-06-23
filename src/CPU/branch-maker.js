34c96d99a36b0e206a7ca2dc54335d59dbc568ed
Alyssa Rosenzweig <alyssa@backtick.town>
2016-06-22 19:16:43 -0700

    Emit branches to stdout

--- src/CPU/branch-maker.js
+++ src/CPU/branch-maker.js
@@ -14,3 +14,27 @@ var branches = {
     "V": ["BVC", "BVS"],
     "N": ["BPL", "BMI"]
 };
+
+function emit(name, flag, value) {
+    var emission = [];
+
+    // header
+    emission.push("BRANCH");
+    emission.push("mapper read PC+1");
+
+    // emission
+    var flagNum = value ? flag : "(1 - " + flag + ")";
+
+    emission.push(
+            "change PC by " +
+            flagNum +
+            " * (M + 256 * (<M < 128> - 1))"
+    );
+
+    console.log(emission.join("\n"));
+}
+
+for(var flag in branches) {
+    emit(branches[flag][0], flag, 0);
+    emit(branches[flag][1], flag, 0);
+}
