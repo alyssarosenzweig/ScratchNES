391454a031ca419bd70b5a4da7367d849f3aa9b3
Alyssa Rosenzweig <alyssa@backtick.town>
2016-06-23 15:47:02 -0700

    Fix bitwise

--- src/CPU/build_bitwise.js
+++ src/CPU/build_bitwise.js
@@ -13,7 +13,8 @@ function emit(op) {
         "R,N,Z,A",
         "set hA to item A+1 of hex",
         "set hOP to item OP+1 of hex",
-        "set A to join (item (join (letter 1 of A) (letter 1 of OP))+1 of " + op + ") (item (join (letter 2 of A) (letter 2 of OP))+1 of " + op + ")"
+        'set A to join "0x" join (item (join "0x" (join (letter 1 of hA) (letter 1 of hOP)))+1 of ' + op + ') (item (join "0x" (join (letter 2 of hA) (letter 2 of hOP)))+1 of ' + op + ')'
+
     ];
 
     fs.writeFileSync("instructions/" + op, emission.join("\n"));
