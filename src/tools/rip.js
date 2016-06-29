1aa86159ec9c8f2878896ac43e896dbaf5efba35
Alyssa Rosenzweig <alyssa@backtick.town>
2016-06-29 15:56:20 -0700

    Trivial tool to rip PRG from NROM .nes files

---
+++ src/tools/rip.js
@@ -0,0 +1,9 @@
+/* rip.js
+ * rips an NROM-32 .nes file
+ */
+
+var fs = require("fs");
+var name = process.argv[2].split(".").slice(0, -1).join(".");
+var game = fs.readFileSync(process.argv[2]);
+var PRGROM = game.slice(16, 16 + 32768);
+fs.writeFileSync(name + ".hex", PRGROM);
