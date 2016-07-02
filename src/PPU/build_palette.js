8fe9de8b94962c562416906e643175508319134a
Alyssa Rosenzweig <alyssa@backtick.town>
2016-07-02 12:26:08 -0700

    I now understand why both Scheme and C matter <3

---
+++ src/PPU/build_palette.js
@@ -0,0 +1,25 @@
+/**
+ * inputs the sRGB palette;
+ * emits the Scratch format lookup
+ */
+
+var fs = require("fs");
+var palette = fs.readFileSync(process.argv[2])
+                .toString()
+                .split("\n")
+                .map(function(str) {
+                    var arr = [];
+                    for(var i = 0; i < str.length; i += 13) {
+                        arr.push(str.slice(i, i+13)
+                                    .trim()
+                                    .split(/[ ]+/)
+                                    .map(function(x) {
+                                        return x*1;
+                                    }));
+                    }
+                    return arr.map(function(x) {
+                        return (65536*x[0]) + (256*x[1]) + x[0];
+                    });
+                });
+
+console.log([].concat.apply([], palette).join("\n"));
