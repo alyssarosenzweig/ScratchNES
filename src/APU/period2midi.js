5d8fee92306fb319cf696400211c0dad8e4fca44
Alyssa Rosenzweig <alyssa@backtick.town>
2016-07-05 11:13:25 -0700

    Fix period table

--- src/APU/period2midi.js
+++ src/APU/period2midi.js
@@ -7,12 +7,26 @@
 var fs = require("fs");
 
 var results = [];
-for(var i = 0; i < 2048; ++i) {
+
+// intentional off-by-one error;
+// the idea here is to prevent Scratch from needing to add 1,
+// which is OK because nobody's going to really play period 0;
+// as a matter of fact, on the real NES periods 0-7 are silent
+
+for(var i = 1; i < 2048; ++i) {
     results.push(mapping(i));
 }
 
+console.log(results.join("\n"));
+
+/* NTSC CPU clock rate */
+var Hz = 1;
+var kHz = 1000 * Hz;
+var MHz = kHz * kHz;
+var CPU = 1.789773 * MHz;
+
 function mapping(period) {
-    var frequency = CPU / (period + 1);
+    var frequency = CPU / ((16*period) + 1);
     var midi = 59 + (12*Math.log(frequency / 440)/Math.log(2));
     return midi;
 }
