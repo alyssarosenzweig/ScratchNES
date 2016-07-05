1016f92a80ba9b18d236e9a1a1b5c4e2938ee0b3
Alyssa Rosenzweig <alyssa@backtick.town>
2016-07-05 11:16:20 -0700

    Misc changes to the APU table

--- src/APU/period2midi.js
+++ src/APU/period2midi.js
@@ -6,27 +6,28 @@
 
 var fs = require("fs");
 
-var results = [];
-
-// intentional off-by-one error;
-// the idea here is to prevent Scratch from needing to add 1,
-// which is OK because nobody's going to really play period 0;
-// as a matter of fact, on the real NES periods 0-7 are silent
-
-for(var i = 1; i < 2048; ++i) {
-    results.push(mapping(i));
-}
-
-console.log(results.join("\n"));
-
 /* NTSC CPU clock rate */
 var Hz = 1;
 var kHz = 1000 * Hz;
 var MHz = kHz * kHz;
 var CPU = 1.789773 * MHz;
 
+var results = [];
+
+// intentional off-by-one error;
+// the idea here is to prevent Scratch from needing to add 1,
+// which is OK because nobody's going to really play period 0;
+// as a matter of fact, on the real NES periods 0-7 are silent
+// Also, this is a %2 table.
+
+for(var i = 1; i < 2048; i += 2) {
+    results.push(mapping(i));
+}
+
+console.log(results.join("\n"));
+
 function mapping(period) {
     var frequency = CPU / ((16*period) + 1);
     var midi = 59 + (12*Math.log(frequency / 440)/Math.log(2));
-    return midi;
+    return Math.round(midi);
 }
